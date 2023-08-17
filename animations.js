255eb5c5f28b59ecae28ed5e51271635d3404c43
AriadneVentura <s3902040@student.rmit.edu.au>
2023-08-17 18:41:28 +1000

    nyoom

--- animations.js
+++ animations.js
@@ -1,19 +1,57 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
 
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+const reveal = () => {
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight
+        const elementTop = reveals[i].getBoundingClientRect().top
+        const elementVisible = 150
 
         if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
+            reveals[i].classList.add("active")
         } else {
-            reveals[i].classList.remove("active");
+            reveals[i].classList.remove("active")
         }
     }
 }
 
 window.addEventListener("scroll", reveal);
 
+
+const typing = () => {
+    const names = ["Ariadne. ", "Endaira. "]
+    let idx = 0
+    let charIdx = 0
+    let direction = 1
+    const elem = document.getElementById("lol")
+    const max = 550;
+    const min = 100;
+
+
+    const handler = () => {
+        // ALLOWED
+        if (direction > 0 ) {
+            elem.innerText += names[idx][charIdx]
+        } else {
+            elem.innerText = elem.innerText.substring(0, elem.innerText.length - 1)
+        }
+
+        // increment the charatcer
+        charIdx += direction
+        if (charIdx === names[idx].length) {
+            direction = -1;
+        } else if (charIdx === 0) {
+            direction = 1;
+            // loop through the array length
+            // NOTE: cool operator
+            idx = (idx + 1) % names.length
+        }
+
+        // dont have handler() bc then ur callingit rn
+        // with just ahndler then u r telling it to be called in the future
+        setTimeout(handler, Math.random() * (max - min) + min)
+    }
+
+    handler();
+}
 
